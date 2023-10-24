1d2e58dd8e3b76a27529cc0efa4ce491ce7e7ca2
CristianoLuo <2012357326@qq.com>
2023-10-24 14:43:52 +0800

    charts colors

---
+++ lib/colors.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+List<Color> colors = [
+  // Color(0xff8e43e7),
+  // Color(0xffff4f81),
+  // Color(0xff1cc7d0),
+  // Color(0xff00aeff),
+  // Color(0xff3369e7),
+  // Color(0xffb84592),
+  // Color(0xff2dde98),
+  // Color(0xffff6c5f),
+  // Color(0xff003666),
+  // Color(0xffffc168),
+  // Color(0xff050f2c),
+  const Color(0xffC73616),
+  const Color(0xffD74D22),
+  const Color(0xffE8651B),
+  const Color(0xffEB882D),
+  const Color(0xff9C60D8),
+  const Color(0xffAB4CCD),
+  const Color(0xff9A20C6),
+  const Color(0xff6B16BF),
+];
