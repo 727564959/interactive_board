e099bb4d3a0d7a13b68e1625078d1f6bcf0a0352
DESKTOP-775ASAC\mzq <15215217793@163.com>
2024-10-23 14:23:32 +0800

    bonus流程

--- lib/data/network/process_controller.dart
+++ lib/data/network/process_controller.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 import 'package:interactive_board/app_routes.dart';
+import 'package:interactive_board/widgets/score_wheel_dialog.dart';
 import '../../common.dart';
 import 'package:interactive_board/data/model/show_state.dart';
 
@@ -13,6 +14,7 @@ class ProcessController {
   factory ProcessController() => _instance ?? ProcessController._internal();
   late final Socket _quizSocket;
   late final Socket _showSocket;
+  late final Socket _bonusSocket;
   bool isChecked = false;
   late ShowState state;
   final _dio = Dio();
@@ -29,6 +31,13 @@ class ProcessController {
       Get.toNamed(AppRoutes.quiz, arguments: data);
     });
     _quizSocket.connect();
+
+    _bonusSocket = io('$baseSocketIoUrl/mini-games/score-wheel', option);
+    _bonusSocket.on('show_trigger', (data) {
+      if (data["teamId"] != Global.tableId) return;
+      Get.dialog(const ScoreWheelDialog(), barrierDismissible: false);
+    });
+    _bonusSocket.connect();
 
     _showSocket = io('$baseSocketIoUrl/listener/game-show', option);
     _showSocket.on('show_state', (data) async {
---
+++ lib/widgets/score_wheel_dialog.dart
@@ -0,0 +1,130 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+import 'package:interactive_board/mirra_style.dart';
+import 'package:pushable_button/pushable_button.dart';
+import 'package:timer_count_down/timer_count_down.dart';
+import 'package:socket_io_client/socket_io_client.dart';
+import 'package:interactive_board/3rd_libs/gif_view-0.4.3/gif_view.dart';
+
+import '../common.dart';
+
+class ScoreWheelDialog extends StatefulWidget {
+  const ScoreWheelDialog({super.key});
+
+  @override
+  State<ScoreWheelDialog> createState() => _ScoreWheelDialogState();
+}
+
+class _ScoreWheelDialogState extends State<ScoreWheelDialog> {
+  int? score;
+  final Socket socket = io(
+    '$baseSocketIoUrl/mini-games/score-wheel',
+    OptionBuilder().setTransports(['websocket']).enableReconnection().disableAutoConnect().build(),
+  );
+  @override
+  void initState() {
+    socket.on('turn_wheel', (data) async {
+      if (data["teamId"] != Global.tableId) return;
+      await Future.delayed(9.seconds);
+      setState(() {
+        score = data["score"];
+      });
+      await Future.delayed(10.seconds);
+      Get.close();
+    });
+    socket.connect();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: Colors.transparent,
+      child: score == null
+          ? _ButtonView()
+          : _ScoreView(
+              score: score!,
+            ),
+    );
+  }
+}
+
+class _ButtonView extends StatelessWidget {
+  _ButtonView();
+  final bCountdown = true.obs;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 1200.w,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          PushableButton(
+            height: 200.w,
+            elevation: 30.w,
+            hslColor: const HSLColor.fromAHSL(1.0, 120, 1.0, 0.37),
+            onPressed: () async {
+              Get.back();
+              await Dio().post("$baseApiUrl/mini-games/score-wheel/turn");
+              bCountdown.value = false;
+            },
+            child: Text(
+              'SPIN',
+              style: CustomTextStyles.button(
+                color: Colors.white,
+                fontSize: 100.sp,
+              ),
+            ),
+          ),
+          Obx(
+            () => bCountdown.value
+                ? Countdown(
+                    seconds: 9,
+                    onFinished: () => bCountdown.value = false,
+                    build: (BuildContext context, double time) => SizedBox(
+                      height: 250.w,
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Text(
+                          (time.toInt() + 1).toString().padLeft(2, '0'),
+                          style: CustomTextStyles.title3(
+                            color: Colors.white,
+                            fontSize: 120.sp,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                : SizedBox(height: 250.w),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ScoreView extends StatelessWidget {
+  const _ScoreView({required this.score});
+  final int score;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        GifView.asset(
+          MirraIcons.getGifPath("show_end_fireworks.gif"),
+          fit: BoxFit.fill,
+        ),
+        Center(
+          child: Text(
+            "+ $score",
+            style: CustomTextStyles.title3(color: Colors.white, fontSize: 300.sp),
+          ),
+        ),
+      ],
+    );
+  }
+}
